c77e3d9764cc297d9ff11d37372c078f103c3438
Cesar <cayocesar.ac@gmail.com>
2020-02-08 03:12:42 -0300

    added login screen

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -11,6 +11,8 @@ class Page{
     private $tpl;
     private $options;
     private $default = [
+        "header"=>true,
+        "footer"=>true,
         "data"=>[]
     ];
 
@@ -32,7 +34,7 @@ class Page{
         $this->setData($this->options["data"]);
         
 
-        $this->tpl->draw("header");
+        if($this->options["header"] === true) $this->tpl->draw("header");
 
     }
 
@@ -49,7 +51,7 @@ class Page{
     }
 
     public function __destruct(){
-        $this->tpl->draw("footer");
+        if($this->options["footer"] === true) $this->tpl->draw("footer");
     }
 
 }
