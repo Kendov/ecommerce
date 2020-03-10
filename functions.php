2a4971d7aabb6a7d12386ed8e92eabcaa1012f0c
Cesar <cayocesar.ac@gmail.com>
2020-03-10 13:44:57 -0300

    admin orders

--- functions.php
+++ functions.php
@@ -10,6 +10,9 @@ function formatPrice($vlprice){
     return number_format($vlprice, 2, ",", ".");
     
 }
+function formatDate($date){
+    return date('d/m/Y', strtotime($date));
+}
 
 function debug_to_console($data) {
     $output = $data;
